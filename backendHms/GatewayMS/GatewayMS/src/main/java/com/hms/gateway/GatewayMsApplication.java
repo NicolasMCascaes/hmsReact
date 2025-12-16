7545916d2811b50f035c7bb62f0dc9f4a84873ba
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-12-15 21:20:06 -0300

    fix: gateway config fixed

--- backendHms/GatewayMS/GatewayMS/src/main/java/com/hms/gateway/GatewayMsApplication.java
+++ backendHms/GatewayMS/GatewayMS/src/main/java/com/hms/gateway/GatewayMsApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayMsApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayMsApplication.class, args);
 	}
+
 }
