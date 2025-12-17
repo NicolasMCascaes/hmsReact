c2ce4f2608446eddbfc899a89fb0664c47cc3eef
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-12-17 09:25:20 -0300

    fix: app routes fixed

--- hms/src/Routes/AppRoutes.tsx
+++ hms/src/Routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Random from '../components/Random'
 import AdminDashboard from '../Layout/AdminDashboard'
 import LoginPage from "../pages/LoginPage"
@@ -7,20 +7,19 @@ import RegisterPage from "../pages/RegisterPage"
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/admin" element={<AdminDashboard />}>
-          <Route path="dashboard" element={<Random />} />
-          <Route path="pharmacy" element={<Random />} />
-          <Route path="doctors" element={<Random />} />
-          <Route path="appointments" element={<Random />} />
-          <Route path="patients" element={<Random />} />
-        </Route>
-      </Routes>
+            <Routes>
+              <Route path='/login' element={<LoginPage/>}/>
+              <Route path="/register" element={<RegisterPage/>}/>
+              <Route path='/' element={<AdminDashboard/>}>
+              <Route path="/dashboard" element={<Random/>} />
+              <Route path="/pharmacy" element={<Random/>} />
+              <Route path="/doctors" element={<Random/>} />
+              <Route path="/appointments" element={<Random/>} />
+              <Route path="/patients" element={<Random/>} />
+              </Route>
+            </Routes>
     </BrowserRouter>
-  );
-};
+  )
+}
+
 export default AppRoutes
