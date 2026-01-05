5c2f27277f246f894d7d6145ad7be44520bbe270
NicolasMCascaes <cascaesnicolas7@gmail.com>
2026-01-04 23:43:35 -0300

    feat: implementing appointments MS

---
+++ backendHms/AppointmentsMS/AppointmentsMS/src/main/java/com/hms/AppointmentsMS/AppointmentsMsApplication.java
@@ -0,0 +1,13 @@
+package com.hms.AppointmentsMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentsMsApplication.class, args);
+	}
+
+}
---
+++ backendHms/AppointmentsMS/AppointmentsMS/src/test/java/com/hms/AppointmentsMS/AppointmentsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.AppointmentsMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
