6a623a8a7edb99adb051de37a909fb1a7288e4fd
NicolasMCascaes <cascaesnicolas7@gmail.com>
2026-04-21 11:21:24 -0300

    feat: implemented logout button at dropdown menu

--- hms/src/components/Header/Header.tsx
+++ hms/src/components/Header/Header.tsx
@@ -1,20 +1,9 @@
-import { ActionIcon, Button } from '@mantine/core'
+import { ActionIcon } from '@mantine/core'
 import { IconBellRinging, IconLayoutSidebarLeftCollapseFilled } from '@tabler/icons-react'
 import ProfileMenu from './ProfileMenu'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { removeJwt } from '../../slices/JwtSlice'
-import { removeUser } from '../../slices/UserSlice'
-
+import {useSelector } from 'react-redux'
 const Header = () => {
   const jwt = useSelector((state:any)=> state.jwt)
-  const dispatch = useDispatch();
-  const handleLogout = () =>{
-    console.log("logout")
-    dispatch(removeJwt())
-    dispatch(removeUser())
-
-  }
   return (
     <div className='bg-light shadow-lg w-full h-16 flex justify-between px-5 items-center'> 
     <ActionIcon variant="transparent" aria-label="Settings" size="xl">
@@ -22,7 +11,6 @@ const Header = () => {
     </ActionIcon>
 
     <div className='flex gap-5 items-center'>
-      {jwt?<Button color='red' onClick={handleLogout}>Logout</Button>:<Link to="login"><Button>Entrar</Button></Link>}
       { jwt&&<><ActionIcon variant="transparent" aria-label="Settings" size="md">
     <IconBellRinging style={{ width: '90%', height: '90%' }} stroke={1.5} />
     </ActionIcon>
--- hms/src/components/Header/ProfileMenu.tsx
+++ hms/src/components/Header/ProfileMenu.tsx
@@ -1,20 +1,26 @@
-import { Menu, Text, Avatar } from '@mantine/core';
+import { Menu, Avatar } from '@mantine/core';
 import {
-  IconSettings,
-  IconSearch,
   IconPhoto,
   IconMessageCircle,
-  IconTrash,
   IconArrowsLeftRight,
+  IconArrowLeft,
 } from '@tabler/icons-react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import avatar from '../../assets/avatar.jpg'
 import { downloadMediaFile } from '../../services/MediaService';
 import { getPatientProfile } from '../../services/PatientProfileService';
 import { getDoctorProfile } from '../../services/DoctorProfileService';
+import { removeUser } from '../../slices/UserSlice';
+import { removeJwt } from '../../slices/JwtSlice';
 
 const ProfileMenu = () => {
+  const dispatch = useDispatch();
+  const handleLogout = () =>{
+    console.log("logout")
+    dispatch(removeJwt())
+    dispatch(removeUser())
+
+  }
   const user = useSelector((state:any)=> state.user)
   const [avatarSrc, setAvatarSrc] = useState<string | null>(null)
 
@@ -49,34 +55,20 @@ const ProfileMenu = () => {
       <Menu.Target>
          <div className='flex items-center gap-3 cursor-pointer'>
            <span className='font-medium text-lg text-neutral-900'>{user.name}</span>
-           <Avatar variant = "filled" src={avatarSrc ?? avatar} alt={user.name} size={50}/>
+           <Avatar variant = "filled" src={avatarSrc} alt={user.name} size={50}/>
          </div>
       </Menu.Target>
 
       <Menu.Dropdown>
         <Menu.Label>Aplicação</Menu.Label>
-        <Menu.Item leftSection={<IconSettings size={14} />}>
-          Configurações
-        </Menu.Item>
+        
         <Menu.Item leftSection={<IconMessageCircle size={14} />}>
           Mensagens
         </Menu.Item>
         <Menu.Item leftSection={<IconPhoto size={14} />}>
           Galeria
         </Menu.Item>
-        <Menu.Item
-          leftSection={<IconSearch size={14} />}
-          rightSection={
-            <Text size="xs" c="dimmed">
-              ⌘K
-            </Text>
-          }
-        >
-          Pesquisar
-        </Menu.Item>
-
         <Menu.Divider />
-
         <Menu.Label>Zona de risco</Menu.Label>
         <Menu.Item
           leftSection={<IconArrowsLeftRight size={14} />}
@@ -85,9 +77,10 @@ const ProfileMenu = () => {
         </Menu.Item>
         <Menu.Item
           color="red"
-          leftSection={<IconTrash size={14} />}
+          leftSection={<IconArrowLeft size={14} />}
+          onClick={handleLogout}
         >
-          Deletar minha conta
+          Sair
         </Menu.Item>
       </Menu.Dropdown>
     </Menu>
